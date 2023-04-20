df4135c83f25988a6a2c12b028f6628d218b910e
NeoHW <neohaowei@gmail.com>
2023-04-21 02:02:52 +0800

    q2 and q3 done with ans

--- PYPs/2223 SEM1/q2.jsh
+++ PYPs/2223 SEM1/q2.jsh
@@ -1,16 +1,22 @@
+import java.util.stream.*;
+
 // Answer 2a below. Do not remove this comment.
-... reverse(...) {
-    return ...
-
-
+<T> ImList<T> reverse (List<? extends T> list) { // use ? extends to make it as general as possible
+    return list.stream()
+        .map(x -> new ImList<T>().add(x)) // because reduce requires seed & element to be the same type
+        .reduce(new ImList<T>() ,(x,y) -> y.addAll(x));
+}
 
 
 
 
 
 // Answer 2b below. Do not remove this comment.
-... pairing(...) {
-    return ...
+<T> List<Pair<T,T>> pairing(List<? extends T> list) { 
+    return IntStream.range(0, list.size() / 2)
+        .mapToObj(x -> new Pair<T,T>(list.get(x * 2), list.get(x * 2 + 1))) // mapToObj as want to convert to general stream
+        .toList();
+}
 
 
 
@@ -19,8 +25,11 @@
 
 
 // Answer 2c below. Do not remove this comment.
-... merging(...) {
-    return ...
+<T> List<T> merging(List<? extends Pair<? extends T, ? extends T>> list) {
+    return list.stream()
+        .flatMap(x -> Stream.of(x.first(), x.second())) // Stream.of() not stream.of()
+        .toList();
+}
 
 
 
--- PYPs/2223 SEM1/q3.java
+++ PYPs/2223 SEM1/q3.java
@@ -1,7 +1,11 @@
+class test{
+    
 // Answer 3a below. Do not remove this comment.
-... getMarks() {
-    return ...
-
+// this returns a stream of all levels of a PA
+Stream<Integer> getMarks() { 
+    return levels.stream()
+        .map(x -> x.getMarks());
+}
 
 
 
@@ -9,18 +13,31 @@
 
 
 // Answer 3b below. Do not remove this comment.
-... getMarks() {
-    return ...
+// This returns a stream of marks of all PAs 
+stream<Optional<Integer>> getMarks() {
+    return listPA.stream() // Stream<Optional<PA>>
+        .map(x -> x.map( // Optional<PA> -> PA
+            y -> y.getMarks() // PA -> Stream<Integer>
+            .reduce(0, (a,b) -> a + b) // Integer
+            ) // Optional<Integer>
+        ); // Stream<Optional<Integer>>
+}
 
-
-
+}
 
 
 
 
 // Answer 3c below. Do not remove this comment.
-... getTotalMarks(...) {
-    return ...
+class Main {
+    int getTotalMarks(List<Student> list) {
+        return list.stream() // Stream<Student>
+            .flatMap(x -> x.getMarks() // Student -> Stream<Optional<Integer>>
+                .map(y -> y.orElse(0)) // Optional<Integer> -> Integer
+            ) // Stream<Integer>
+            .reduce(0, (x,y) -> x + y);
+    }
+}
 
 
 
