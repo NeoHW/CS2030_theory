50b7239a39a2e48b1032efa71f3f42e32e5d1a2a
NeoHW <neohaowei@gmail.com>
2023-04-23 13:05:38 +0800

    q4: annoymous inner class

--- PYPs/2122 SEM1/q4.java
+++ PYPs/2122 SEM1/q4.java
@@ -5,27 +5,32 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 abstract class MyStream<T> {
+
     static <T> MyStream<T> generate(Supplier<T> seed) {
         return new MyStream<T>() {
             public T get() {
                 return seed.get();
             }
+
+            // a
+            void forEach(Consumer<? super T> action, int n) {
+                Stream.generate(seed)
+                        .limit(n)
+                        .forEach(action);
+            }
+
+            // b ???
+            <R> MyStream<T> map(Function<? extends T, ? extends R> mapper) {
+                Stream<T> stream = Stream.generate(seed)
+                    .map(mapper)
+                    .
+            }
+
         };
     }
 
     abstract T get();
-}
+    abstract void forEach(Consumer<? super T> action, int n);
+    abstract <R> MyStream<T> map(Function<? extends T, ? extends R> mapper);
 
-// a
-void forEach(Consumer<? super T> action , int n) {
-    Stream<T> stream = Stream.generate(seed)
-        .limit(n)    
-        .forEach(action);
-}
-
-// b  ???
-<R> MyStream<T> map(Function<? extends T, ? extends R> mapper) {
-    Stream<T> stream = Stream.generate(seed)
-        .map(mapper)
-        .
 }
