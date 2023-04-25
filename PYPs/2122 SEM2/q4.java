2a05ba03df9fd3f2b6955a2bd32fdd30c253312d
NeoHW <neohaowei@gmail.com>
2023-04-25 20:05:28 +0800

    IfElse context

--- PYPs/2122 SEM2/q4.java
+++ PYPs/2122 SEM2/q4.java
@@ -1,39 +1,56 @@
 import java.util.function.*;
 
-class IfElse<T,R> {
+class IfElse<T,U> {
 
     // a
     private final Predicate<T> pred;
-    private final Function<? super T,? extends R> mapper;
-    private final Function<? super T,? extends R> mapper2;
+    private final Function<T,U> left;
+    private final Function<T,U> right;
 
-    IfElse(Predicate<T> pred, Function<? super T,? extends R> mapper, Function<? super T,? extends R> mapper2) {
+    IfElse(Predicate<T> pred, Function<T,U> left, Function<T,U> right) {
         this.pred = pred;
-        this.mapper = mapper;
-        this.mapper2 = mapper2;
+        this.left = left;
+        this.right = right;
     }
 
-    static <T,R> IfElse<T,R> of(Predicate<T> pred, Function<? super T,? extends R> mapper, Function<? super T,? extends R> mapper2) {
-        return new IfElse<T,R>(pred, mapper, mapper2);
+    static <T,U> IfElse<T,U> of(Predicate<T> pred, Function<T,U> left, Function<T,U> right) {
+        return new IfElse<T,U>(pred, left, right);
     }
 
-    R apply(T t) {
-        return pred.test(t) ? mapper.apply(t) : mapper2.apply(t) ;
+    U apply(T t) {
+        return pred.test(t) ? left.apply(t) : right.apply(t) ;
     }
     
     //b
-    IfElse<T,R> mapIf(IfElse<T,R> other) {
-        return new IfElse<T,R>(x -> this.pred.test(x) && other.pred.test(x), mapper, mapper2);
+    IfElse<T,U> mapIf(IfElse<T,U> other) {
+        // return new IfElse<T,U>(pred, x -> other.apply(x) , right);
+        // ^ the statement above is wrong as f.mapIf(g).mapIf(h).apply(2) ≠ f.mapIf(g.mapIf(h)).apply(2) 
+        return new IfElse<T,U>(pred.and(other.pred), other.left, // if both are true, use other.left (draw diagram out)
+            x -> new IfElse<T,U>(pred, other.right, this.right).apply(x));
     }
 
     //c 
-    IfElse<T,R> mapElse(IfElse<T,R> other) {
-        return new IfElse<T,R>(x -> this.pred.test(x) || other.pred.test(x), mapper, mapper2);
+    IfElse<T,U> mapElse(IfElse<T,U> other) {
+        return new IfElse<T,U>(pred.or(other.pred), // use same diagram to see
+            x -> new IfElse<T,U>(pred, left, other.left).apply(x),
+            other.right);
     }
 
-    //d (i do not know how to do this)
-    IfElse<T,R> map(Function<? super T,? extends R> mapper) {
-        return new IfElse<T,R>(this.pred, x -> mapper.test(true), mapper.test(!(true)));
+    //d 
+    // use andTHen to compose 2 functions
+    <R> IfElse<T,R> map(Function<? super U,? extends R> mapper) {
+        return new IfElse<T,R>(this.pred, left.andThen(mapper), right.andThen(mapper));
+    }
+
+    <R> IfElse<T,R> flatMap(Function<? super U,? extends IfElse<U, ? extends R>> mapper) {
+        
+        Function<T,R> f = x -> mapper.apply(this.apply(x)).apply(this.apply(x));
+        return new IfElse<T,R>(pred, f, f);
+        /*
+        return new IfElse<T,R>(this.pred,
+        x -> mapper.apply(left.apply(x)).apply(left.apply(x)),
+        x -> mapper.apply(right.apply(x)).apply(right.apply(x)));
+        */
     }
 }
 
