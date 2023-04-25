5e391fd8502e5c00724a3359077fb357e93874b8
NeoHW <neohaowei@gmail.com>
2023-04-25 22:41:29 +0800

    annoymous inner class with MyStream context

--- PYPs/2122 SEM1/q4.java
+++ PYPs/2122 SEM1/q4.java
@@ -1,36 +1,40 @@
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 abstract class MyStream<T> {
 
+    // try not to use streams as it then defeats the purposes of implementing myStream
     static <T> MyStream<T> generate(Supplier<T> seed) {
         return new MyStream<T>() {
+            
             public T get() {
                 return seed.get();
             }
-
-            // a
-            void forEach(Consumer<? super T> action, int n) {
-                Stream.generate(seed)
-                        .limit(n)
-                        .forEach(action);
-            }
-
-            // b ???
-            <R> MyStream<T> map(Function<? extends T, ? extends R> mapper) {
-                Stream<T> stream = Stream.generate(seed)
-                    .map(mapper)
-                    .
-            }
-
         };
     }
 
     abstract T get();
-    abstract void forEach(Consumer<? super T> action, int n);
-    abstract <R> MyStream<T> map(Function<? extends T, ? extends R> mapper);
+
+    // a
+    void forEach(Consumer<? super T> action, int n) {
+        for (int i = 0; i < n; i++) {
+            action.accept(this.get());
+        }
+    }
+
+    // b
+    // map() should return a MyStream so that it can be chained with forEach()
+    <R> MyStream<R> map(Function<? super T, ? extends R> mapper) {
+        return new MyStream<R>() {
+            public R get() {
+                // must use MyStream.this.get()
+                // if not this.get() refers to own method and it will be infinite loop
+                return mapper.apply(MyStream.this.get());
+            }
+        };
+    }
 
 }
+
+
